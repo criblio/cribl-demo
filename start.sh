ee9d3053bd02e2f7dec60eb3e7821c11c97567e6
Clint Sharp <clint@cribl.io>
2021-02-26 18:22:22 +0000

    Removing noscope

--- start.sh
+++ start.sh
@@ -6,5 +6,7 @@ cd ${DIR}
 
 [ "$1" = "local" ] && minikube start $*
 
-source ${DIR}/mutating-webhook/scripts/install.sh
+./scope k8s --metricdest tcp://cribl-w1:8125 --metricformat statsd --eventdest tcp://cribl-w0:10070 | kubectl apply -f -
+
+# source ${DIR}/mutating-webhook/scripts/install.sh
 # cd ${DIR} && exec skaffold dev --port-forward=true
