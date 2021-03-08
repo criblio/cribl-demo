0c7efc8bd6c5e93ffbc25b35ec9835a912150184
Steve Litras <steve@cribl.io>
2021-03-08 07:47:01 -0800

    doing file check for scope prior to trying to kick it off

--- start.sh
+++ start.sh
@@ -6,7 +6,9 @@ cd ${DIR}
 
 [ "$1" = "local" ] && minikube start $*
 
-./scope k8s --metricdest tcp://cribl-w1:8125 --metricformat statsd --eventdest tcp://cribl-w0:10070 | kubectl apply -f -
+if [ -f ./scope ]; then
+  ./scope k8s --metricdest tcp://cribl-w1:8125 --metricformat statsd --eventdest tcp://cribl-w0:10070 | kubectl apply -f -
+fi
 
 # source ${DIR}/mutating-webhook/scripts/install.sh
 # cd ${DIR} && exec skaffold dev --port-forward=true
