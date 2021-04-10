dc298067500944e9e1481d57af663f1cb9021947
Steve Litras <steve@cribl.io>
2021-04-10 23:09:26 +0000

    fixing paths

--- scripts/deploy_envs.sh
+++ scripts/deploy_envs.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(dirname $BASH_SOURCE[0])
+DIR="$((cd -P "$SCRIPT_DIR/..") ; pwd)"
+
 arg=$1
 if [ -z "$arg" ]; then
   echo "Usage: $0 <job tag>"
   exit 254
 fi
 
-for dom in $(./enumerate_envs.py -j $arg); do 
+for dom in $($DIR/enumerate_envs.py -j $arg); do 
   # Remove kubeconfig
   #if [ -e ${HOME}/.kube/config ]; then 
    # rm ${HOME}/.kube/config
@@ -15,6 +18,6 @@ for dom in $(./enumerate_envs.py -j $arg); do
   cluster=$(echo $dom | awk -F- '{print $2}'); 
   echo "NS: $ns, Cluster $cluster"
   aws eks update-kubeconfig --name $cluster
-  ./undeploy-eks.py -n $ns
-  ./deploy-eks.py -n $ns
+  $DIR/undeploy-eks.py -n $ns
+  $DIR/deploy-eks.py -n $ns
 done
