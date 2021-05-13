b27ec82ce64bbd830936718aa19cb0d564168aa8
Steve Litras <steve@cribl.io>
2021-05-13 10:53:26 -0700

    changing to using cribl CLI

--- deploy-job/api-script.sh
+++ deploy-job/api-script.sh
@@ -2,19 +2,24 @@
 
 # This works off a secret exposed as an env var in the pod CRIBL_ADMIN_PASSWORD
 if [ -z "$CRIBL_ADMIN_PASSWORD" ] || [ -z "$CRIBL_URL" ]; then
-if [ -z "$CRIBL_ADMIN_PASSWORD" ]; then
-  CRIBL_ADMIN_PASSWORD=l0gstr3am
+  echo "Both CRIBL_ADMIN_PASSWORD AND CRIBL_URL need to be set"
+  exit 255
 fi
 
-if [ -z "$CRIBL_URL" ]; then
-  CRIBL_URL=http://cribl.logstreamnext.demo.cribl.io:9000
+if [ -z "$CRIBL_HOME" ]; then
+  echo "No CRIBL_HOME Set - defaulting to /opt/cribl"
+  export CRIBL_HOME=/opt/cribl
 fi
 
-export TOKEN=$(curl -sq -X POST "$CRIBL_URL/api/v1/auth/login" \
-               -H  "accept: application/json" \
-               -H  "Content-Type: application/json" \
-               -d "{\"username\":\"admin\",\"password\":\"$CRIBL_ADMIN_PASSWORD\"}" |\
-               jq '.token' -r)
+if [ -n "$CRIBL_VOLUME_DIR" ]; then
+  export CRIBL_CONF_DIR=$CRIBL_VOLUME_DIR
+fi
+
+$CRIBL_HOME/bin/cribl auth login -u admin -p $CRIBL_ADMIN_PASSWORD -h $CRIBL_URL
+if [ $? -gt 0 ]; then
+  echo "Failed LogStream Login"
+  exit 254
+fi
 
 
 while IFS=, read -r pack group; do
@@ -24,46 +29,21 @@ while IFS=, read -r pack group; do
       last=${#CRIBL_GROUPS[@]}
       CRIBL_GROUPS[last]=$group
     fi
-    # Upload the crbl file
-    PACK_SOURCE=$(curl -s -X PUT \
-         -T ../packs/build/$pack $CRIBL_URL/api/v1/m/$group/packs\?filename\=$pack \
-         -H "accept: application/json" \
-         -H "Authorization: Bearer $TOKEN" |\
-    jq '.source' -r)
-    # Install the pack
-    STATUS=$(curl -s -X POST -d "{\"source\": \"$PACK_SOURCE\"}" \
-         -H "Content-Type: application/json" \
-         -H "Authorization: Bearer $TOKEN"  $CRIBL_URL/api/v1/m/$group/packs | \
-    jq '.count' -r)
+    $CRIBL_HOME/bin/cribl pack install -d -g $group ../packs/build/$pack
+    if [ $? -gt 0 ]; then
+      echo "Pack: $pack install failed on $group"
+    else
+      echo "Pack: $pack installed on $group"
+    fi
     echo "Status: $STATUS"
   fi
 done < pack-manifest.csv
 
-#echo ${CRIBL_GROUPS[@]}
 
 
-commit=$(curl -s -X POST "$CRIBL_URL/api/v1/version/commit" \
-              -H  "accept: application/json" \
-              -H  "Authorization: Bearer $TOKEN" |\
-         jq -r '.items[0].commit')
-if [ -z "$commit" ]; then
-  echo "No Commit Found, exiting"
-  exit 255
-fi
 for grp in ${CRIBL_GROUPS[@]}; do
-
   echo "Deploying $grp"
-  patchdata="{\"version\": \"$commit\", \"id\": \"$grp\"}"
-  # Run commit and deploy separately for each worker group (commit-deploy will fail if
-  # there is nothing to commit, but we want to push a deploy either way.
-  echo $patchdata > /tmp/patch$$
-  #echo "pdata: $patchdata"
-  STATUS=$(curl -s -X PATCH -d@/tmp/patch$$ "$CRIBL_URL/api/v1/master/groups/$grp/deploy" \
-       -H "accept: application/json" \
-       -H "Authorization: Bearer $TOKEN" \
-       -H "Content-Type: application/json" |\
-  jq '.items[0].configVersion' -r)
-  echo "Deployed $STATUS on $grp"
-  rm /tmp/patch$$
+  $CRIBL_HOME/bin/cribl groups commit -g $grp -m "init"
+  $CRIBL_HOME/bin/cribl groups deploy -g $grp
 done
 
