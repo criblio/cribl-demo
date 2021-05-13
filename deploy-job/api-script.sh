03f7d8f693267876e04a652255c9b474be2df554
Steve Litras <steve@cribl.io>
2021-05-13 13:07:44 -0700

    going back to API for now, since remote file upload is not supported yet in the CLI

--- deploy-job/api-script.sh
+++ deploy-job/api-script.sh
@@ -6,22 +6,15 @@ if [ -z "$CRIBL_ADMIN_PASSWORD" ] || [ -z "$CRIBL_URL" ]; then
   exit 255
 fi
 
-if [ -z "$CRIBL_HOME" ]; then
-  echo "No CRIBL_HOME Set - defaulting to /opt/cribl"
-  export CRIBL_HOME=/opt/cribl
-fi
-
-if [ -n "$CRIBL_VOLUME_DIR" ]; then
-  export CRIBL_CONF_DIR=$CRIBL_VOLUME_DIR
-fi
-
-$CRIBL_HOME/bin/cribl auth login -u admin -p $CRIBL_ADMIN_PASSWORD -h $CRIBL_URL
-if [ $? -gt 0 ]; then
-  echo "Failed LogStream Login"
-  exit 254
-fi
+# Authenticate against the API
+export TOKEN=$(curl -sq -X POST "$CRIBL_URL/api/v1/auth/login" \
+               -H  "accept: application/json" \
+               -H  "Content-Type: application/json" \
+               -d "{\"username\":\"admin\",\"password\":\"$CRIBL_ADMIN_PASSWORD\"}" |\
+               jq '.token' -r)
 
 
+# Read the Manifest File and install each pack specified in it. 
 while IFS=, read -r pack group; do
   if [[ "$pack" != "pack" ]]; then
     echo "Installing $pack in $group"
@@ -29,21 +22,47 @@ while IFS=, read -r pack group; do
       last=${#CRIBL_GROUPS[@]}
       CRIBL_GROUPS[last]=$group
     fi
-    $CRIBL_HOME/bin/cribl pack install -d -g $group ../packs/build/$pack
-    if [ $? -gt 0 ]; then
-      echo "Pack: $pack install failed on $group"
-    else
-      echo "Pack: $pack installed on $group"
-    fi
+    # Upload the crbl file
+    PACK_SOURCE=$(curl -s -X PUT \
+         -T ../packs/build/$pack $CRIBL_URL/api/v1/m/$group/packs\?filename\=$pack \
+         -H "accept: application/json" \
+         -H "Authorization: Bearer $TOKEN" |\
+    jq '.source' -r)
+    # Install the pack
+    STATUS=$(curl -s -X POST -d "{\"source\": \"$PACK_SOURCE\"}" \
+         -H "Content-Type: application/json" \
+         -H "Authorization: Bearer $TOKEN"  $CRIBL_URL/api/v1/m/$group/packs | \
+    jq '.count' -r)
     echo "Status: $STATUS"
   fi
 done < pack-manifest.csv
 
+#echo ${CRIBL_GROUPS[@]}
 
 
+commit=$(curl -s -X POST "$CRIBL_URL/api/v1/version/commit" \
+              -H  "accept: application/json" \
+              -H  "Authorization: Bearer $TOKEN" |\
+         jq -r '.items[0].commit')
+if [ -z "$commit" ]; then
+  echo "No Commit Found, exiting"
+  exit 255
+fi
 for grp in ${CRIBL_GROUPS[@]}; do
+
   echo "Deploying $grp"
-  $CRIBL_HOME/bin/cribl groups commit -g $grp -m "init"
-  $CRIBL_HOME/bin/cribl groups deploy -g $grp
+  patchdata="{\"version\": \"$commit\", \"id\": \"$grp\"}"
+  # Run commit and deploy separately for each worker group (commit-deploy will fail if
+  # there is nothing to commit, but we want to push a deploy either way.
+  echo $patchdata > /tmp/patch$$
+  #echo "pdata: $patchdata"
+  STATUS=$(curl -s -X PATCH -d@/tmp/patch$$ "$CRIBL_URL/api/v1/master/groups/$grp/deploy" \
+       -H "accept: application/json" \
+       -H "Authorization: Bearer $TOKEN" \
+       -H "Content-Type: application/json" |\
+  jq '.items[0].configVersion' -r)
+  echo "Deployed $STATUS on $grp"
+  rm /tmp/patch$$
 done
+
 
